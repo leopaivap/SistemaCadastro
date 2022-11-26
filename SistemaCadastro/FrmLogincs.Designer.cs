9f64a2ee89252a1db6a6a40ebdaab36b28625845
Leo <paivaleo7@gmail.com>
2022-11-26 09:08:22 -0300

    login estatico

---
+++ SistemaCadastro/FrmLogincs.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace SistemaCadastro
+{
+    partial class FrmLogincs
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmLogincs));
+            this.btnFechar2 = new System.Windows.Forms.Button();
+            this.txtUserLogin = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtSenhaLogin = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnFechar2
+            // 
+            this.btnFechar2.FlatAppearance.BorderSize = 0;
+            this.btnFechar2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFechar2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnFechar2.ForeColor = System.Drawing.Color.White;
+            this.btnFechar2.Image = global::SistemaCadastro.Properties.Resources.close;
+            this.btnFechar2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnFechar2.Location = new System.Drawing.Point(724, -1);
+            this.btnFechar2.Margin = new System.Windows.Forms.Padding(5);
+            this.btnFechar2.Name = "btnFechar2";
+            this.btnFechar2.Size = new System.Drawing.Size(59, 65);
+            this.btnFechar2.TabIndex = 10;
+            this.btnFechar2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnFechar2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnFechar2.UseVisualStyleBackColor = true;
+            this.btnFechar2.Click += new System.EventHandler(this.btnFechar2_Click);
+            // 
+            // txtUserLogin
+            // 
+            this.txtUserLogin.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtUserLogin.Location = new System.Drawing.Point(221, 251);
+            this.txtUserLogin.Margin = new System.Windows.Forms.Padding(5);
+            this.txtUserLogin.Name = "txtUserLogin";
+            this.txtUserLogin.Size = new System.Drawing.Size(411, 34);
+            this.txtUserLogin.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.Color.DimGray;
+            this.label4.Location = new System.Drawing.Point(107, 258);
+            this.label4.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 23);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Usuário:";
+            // 
+            // txtSenhaLogin
+            // 
+            this.txtSenhaLogin.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtSenhaLogin.Location = new System.Drawing.Point(221, 325);
+            this.txtSenhaLogin.Margin = new System.Windows.Forms.Padding(5);
+            this.txtSenhaLogin.Name = "txtSenhaLogin";
+            this.txtSenhaLogin.PasswordChar = '*';
+            this.txtSenhaLogin.Size = new System.Drawing.Size(411, 34);
+            this.txtSenhaLogin.TabIndex = 13;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.FlatAppearance.BorderSize = 0;
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogin.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnLogin.ForeColor = System.Drawing.Color.DimGray;
+            this.btnLogin.Image = ((System.Drawing.Image)(resources.GetObject("btnLogin.Image")));
+            this.btnLogin.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnLogin.Location = new System.Drawing.Point(305, 403);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(5);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(117, 75);
+            this.btnLogin.TabIndex = 14;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnLogin.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(294, 46);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(167, 134);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 16;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.DimGray;
+            this.label1.Location = new System.Drawing.Point(107, 332);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 23);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Senha:";
+            // 
+            // FrmLogincs
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(782, 615);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtSenhaLogin);
+            this.Controls.Add(this.txtUserLogin);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnFechar2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FrmLogincs";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FrmLogincs";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFechar2;
+        private System.Windows.Forms.TextBox txtUserLogin;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtSenhaLogin;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ SistemaCadastro/FrmLogincs.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaCadastro
+{
+    public partial class FrmLogincs : Form
+    {
+        public FrmLogincs()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFechar2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if(txtUserLogin.Text.Equals("adm") && txtSenhaLogin.Text.Equals("123"))
+            {
+                Sistema sistema = new Sistema();
+                this.Hide();
+                sistema.ShowDialog();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Usuário ou Senha incorreta!");
+            }
+            
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
